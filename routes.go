f835246b6d98fb2f2eb85e9605110e69c621ba36
Darkclainer <Darkclainer@gmail.com>
2019-10-06 18:12:05 +0300

    Add /messages/add to router

--- routes.go
+++ routes.go
@@ -3,4 +3,5 @@ package main
 func (s *Server) routes() {
 	s.router.HandleFunc("/users/add", s.handleAddUser()).Methods("POST")
 	s.router.HandleFunc("/chats/add", s.handleAddChat()).Methods("POST")
+	s.router.HandleFunc("/messages/add", s.handleAddMessage()).Methods("POST")
 }
